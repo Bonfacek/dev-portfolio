9cff05c048197ba855422c303ad3d0da1dc7e27f
~Cyberman~ <karaninjoroge005@gmail.com>
2026-04-14 23:12:33 +0300

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -161,7 +161,7 @@ const Hero: React.FC = () => {
             }}
           >
             <img
-              src={`${import.meta.env.BASE_URL}icon.png`}
+              src="/dev-portfolio/icon.png"
               alt="Karanis"
               className="w-full h-full object-cover"
             />
